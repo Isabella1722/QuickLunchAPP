2bdf10fcf455b7abcdfeee0f162624aa3741a4a9
Isabella Linares <isabellalinarescamacho0517@gmail.com>
2020-05-13 19:49:25 -0500

    se realizó la pantalla de registro

--- app/src/main/java/com/example/quicklunchapp/RegistroActivity.java
+++ app/src/main/java/com/example/quicklunchapp/RegistroActivity.java
@@ -3,12 +3,32 @@ package com.example.quicklunchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistroActivity extends AppCompatActivity {
 
+
+    private EditText nombreET;
+    private EditText codigoET;
+    private EditText documentoET;
+    private EditText claveUnoET;
+    private EditText claveDosET;
+    private Button registrarseBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        nombreET= findViewById(R.id.nombreET);
+        codigoET= findViewById(R.id.codigoET);
+        documentoET= findViewById(R.id.documentoET);
+        claveUnoET= findViewById(R.id.claveUnoET);
+        claveDosET= findViewById(R.id.claveDosET);
+        registrarseBtn= findViewById(R.id.registrarseBtn);
+
+
+
+
+
     }
 }
