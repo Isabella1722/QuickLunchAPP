3e08e75faa67a80be5aa66d08013e57a174b8d11
Isabella Linares <isabellalinarescamacho0517@gmail.com>
2020-05-13 19:02:25 -0500

    se realizó la pantalla inicial

--- app/src/main/java/com/example/quicklunchapp/MainActivity.java
+++ app/src/main/java/com/example/quicklunchapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.quicklunchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button iniciarSesionBtn;
+    private Button registroBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        iniciarSesionBtn= findViewById(R.id.iniciarSesionBtn);
+        registroBtn= findViewById(R.id.registroBtn);
+
+
+
+        iniciarSesionBtn.setOnClickListener(
+                (v)->{
+                    Intent i = new Intent(this,IniciarSesionActivity.class);
+                    startActivity(i);
+                }
+        );
+
+        registroBtn.setOnClickListener(
+                (v)->{
+                    Intent i = new Intent(this,RegistroActivity.class);
+                    startActivity(i);
+                }
+        );
+
     }
 }
--- app/src/main/java/com/example/quicklunchapp/MenuActivity.java
+++ app/src/main/java/com/example/quicklunchapp/MenuActivity.java
@@ -2,13 +2,25 @@ package com.example.quicklunchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+
+
+
     }
+
+
 }
